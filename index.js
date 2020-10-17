4d00cda04c258aa48c0f2e738af215b409e95f75
Ramon Mesquita <ramon.mesquita@zoom.com.br>
2020-10-17 14:36:26 -0300

    Get the random coins

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const credentials = require("./credentials.json");
 
   await page.title(
     "Shopee Brasil | Ofertas incríveis. Melhores preços do mercado"
-  );
+  );  
 
   await page.type('input[name="loginKey"]', credentials.username, {
     delay: 100,
@@ -39,6 +39,20 @@ const credentials = require("./credentials.json");
   } catch {
     console.log("Daily Coins got!");
   }
+
+  await page.goto('https://shopee.com.br/pc_event/?url=https%3A%2F%2Fgames.shopee.com.br%2Fluckydraw%2Fbox%2Factivity%2F98e3907a3419e0e4');
+  
+  await page.title('Shopee Brasil | Ofertas incríveis. Melhores preços do mercado');
+
+  await page.waitFor(10000);
+
+  const frames = await page.frames();
+
+  console.log(frames[1]);
+
+  const goldenEgg = await frames[1].$('div#clickArea');
+
+  await goldenEgg.click();
 
   await browser.close();
 })();
